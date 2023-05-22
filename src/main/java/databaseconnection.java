5791539f7f87d87e5b5111fb43e47be1691619e9
ahmedkhaled200188 <132754914+ahmedkhaled200188@users.noreply.github.com>
2023-05-22 21:45:38 +0300

    Add files via upload

---
+++ src/main/java/databaseconnection.java
@@ -0,0 +1,25 @@
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+public class databaseconnection {
+    final static String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+    final static String DB_URL = "jdbc:mysql://localhost:3306/student";
+    final static String USER ="root";
+    final static String PASS = "";
+    final static Connection Connection ()
+       {
+           try{
+               Class.forName(JDBC_DRIVER);
+               
+               Connection conn = DriverManager.getConnection(DB_URL,USER,PASS);
+               return  conn;
+           }catch(ClassNotFoundException | SQLException e )
+           {
+               JOptionPane.showMessageDialog(null, e);
+               return null;
+           }
+       }
+}
